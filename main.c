9b18b3746124965c37064fe7ff0e7c43abef9638
alienware coolpunch <alienwarecoolpunch@gmail.com>
2019-05-18 21:59:49 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("editing this file in start branch")
+    printf("this change is done in start1 branch")
+    return 0;
+}
---
+++ name.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("my name is ayub \n");
+    return 0;
+}
